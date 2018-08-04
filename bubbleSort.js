16e2d89fabf56ae0c9cd572f067274b431091633
Alejandro R <Alexrom789@Gmail.com>
2018-08-04 13:41:24 -0400

    Bubble Sort

---
+++ bubbleSort.js
@@ -0,0 +1,28 @@
+// Bubble Sort Practice.
+// Time Complexity: Best-O(n) Avg-O(n^2) Worse- O(n^2)
+// Space Complexity: O(1)
+
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function bubbleSort(array){
+  let swapped;
+    do {
+        swapped = false;
+        for (let i =0; i < array.length; i++){
+          if (array[i] && array[i + 1] && array[i] > array[i + 1]){
+              swap(array, i, i + 1);
+              swapped = true;
+          }
+        }
+
+    } while(swapped);
+  return array;
+}
+
+const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+console.log('Before Sort: ', array);
+console.log('After Sort', bubbleSort(array));
