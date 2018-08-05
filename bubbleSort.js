afcac892b50f1f9d20d187744f93f548c9be0d1f
Alejandro R <Alexrom789@Gmail.com>
2018-08-05 08:30:43 -0400

    refactored bbsort

--- bubbleSort.js
+++ bubbleSort.js
@@ -12,8 +12,8 @@ function bubbleSort(array){
   let swapped;
     do {
         swapped = false;
-        for (let i =0; i < array.length; i++){
-          if (array[i] && array[i + 1] && array[i] > array[i + 1]){
+        for (let i =0; i < array.length - 1; i++){
+          if (array[i] > array[i + 1]){
               swap(array, i, i + 1);
               swapped = true;
           }
